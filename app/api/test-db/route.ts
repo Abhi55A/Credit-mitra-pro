e3e07f4a17c7f4b6b3392b723b9dda3305b209f2
Abhijit Mandal <am0885221@gmail.com>
2026-07-16 16:27:49 +0530

    Test Vercel environment variables

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -1,25 +1,9 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
 
 export async function GET() {
-  try {
-    await prisma.$connect();
-
-    return NextResponse.json({
-      success: true,
-      message: "Database Connected",
-    });
-  } catch (error: any) {
-    console.error(error);
-
-    return NextResponse.json(
-      {
-        success: false,
-        error: error.message,
-      },
-      {
-        status: 500,
-      }
-    );
-  }
+  return NextResponse.json({
+    DATABASE_URL: process.env.DATABASE_URL ? "FOUND" : "NOT FOUND",
+    DIRECT_URL: process.env.DIRECT_URL ? "FOUND" : "NOT FOUND",
+    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL ? "FOUND" : "NOT FOUND",
+  });
 }
